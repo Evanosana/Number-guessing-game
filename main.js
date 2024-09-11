aa991d20c445d37ee47933ba6c3415a08a201f3c
StudentPM <studentpm@Administrators-MacBook-Pro.local>
2024-09-11 15:41:56 -0700

    done except for flash effect

--- main.js
+++ main.js
@@ -1,21 +1,74 @@
-let input = document.getElementById("input")
-const submitButton = document.getElementById("submit")
-let randomNumber = Math.floor(Math.random() * 25)
+let submitButton = document.getElementById("submit");
+let randomNumber = Math.floor(Math.random() * 25);
+let changeText = document.getElementById("change")
+let backgroundFlash = document.getElementById("flasher")
+let num;
 
-submitButton.addEventListener("click", guessButton)
+submitButton.addEventListener("click", decideFunction);
 
-console.log(randomNumber)
+console.log(randomNumber);
 
+function decideFunction(){
+    if(submitButton.innerText === "reset"){
+        resetGame();
+    }
+    guessButton();
+    
+}
     
 function guessButton(){
 
-    if(input > randomNumber){
+    num = document.getElementById("input").value;
+
+    if(num === ""){
+        changeText.innerText = "Guess:"
+        changeText.style.color = "black"
+        changeText.style.fontWeight = "normal"
+        changeText.style.fontSize = "16px"
+        return
+    }
+    num = parseInt(document.getElementById("input").value);
+
+    if(num > randomNumber){
+        changeText.innerText = "Guess lower!"
+        changeText.style.color = "red"
+        changeText.style.fontSize = "20px"
+        triggerFlash()
         console.log("lower")
     } 
-    else if(input === randomNumber){
+    else if(num === randomNumber){
+        changeText.innerText = "GG you won!"
+        changeText.style.color = "green"
+        changeText.style.fontWeight = "bold"
+        changeText.style.fontSize = "24px"
+        submitButton.innerText = "reset"
         console.log("win")
     }
     else{
+        changeText.innerText = "Guess higher!"
+        changeText.style.color = "red"
+        changeText.style.fontSize = "20px"
         console.log("higher")
+        triggerFlash()
     }
+}
+
+function triggerFlash(){
+
+    backgroundFlash.classList.add("flash");
+    changeText.classList.add("flashTwo")
+    setTimeout(() => {
+        backgroundFlash.classList.remove("flash");
+    }, 400);
+    setTimeout(() => {
+        backgroundFlash.classList.remove("flashTwo");
+    }, 400);
+}
+
+function resetGame(){
+
+    randomNumber = Math.floor(Math.random() * 25);
+    submitButton.innerText = "guess"
+    document.getElementById("input").value = ""; // Clear the input field
+    console.log(randomNumber)
 }
